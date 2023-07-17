65fcd23278fa3c0525fcc1fd09fc3e73850a02f3
Matheus Moreira <matheus@MacBook-Air-de-Macbook.local>
2023-07-17 16:05:53 -0300

    Criação do projeto sem storyboard

---
+++ CatsGallery/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CatsGallery
+//
+//  Created by Matheus Moreira on 17/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
+
---
+++ CatsGalleryUITests/CatsGalleryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CatsGalleryUITestsLaunchTests.swift
+//  CatsGalleryUITests
+//
+//  Created by Matheus Moreira on 17/07/23.
+//
+
+import XCTest
+
+final class CatsGalleryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
